b8d2b479afc59bc622ecda08ab41c30b815b691a
Christopher Todd <Christopher.Todd@edu.sait.ca>
2021-01-06 11:20:41 -0700

    code cleanup and security updates

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
-const knex = require('knex')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 //connect server to db using knex npm package
 const db = knex({
@@ -16,10 +20,6 @@ const db = knex({
     }
 });
 
-db.select('*').from('users').then(data => {
-    //console.log(data);
-});
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -28,79 +28,27 @@ app.get('/', (req, res) => {
     res.send(database.users);
 })
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-      .where('email', '=', req.body.email)
-      .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if (isValid) {
-          return db.select('*').from('users')
-            .where('email', '=', req.body.email)
-            .then(user => {
-              res.json(user[0])
-            })
-            .catch(err => res.status(400).json('unable to get user'))
-        } else {
-          res.status(400).json('wrong credentials')
-        }
-      })
-      .catch(err => res.status(400).json('wrong credentials'))
-  })
-
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*') //knex returns all users
-                    .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
+app.post('/signin', (req, res) => { 
+    signin.handleSignin(req, res, db, bcrypt)
 })
 
-app.get('/profile/:id', (req, res) => { //loop through to display the logged in user
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-        .then(user => {
-            if (user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(400).json('not found!')
-            }
-            
-        })
-        .catch(err => res.status(400).json('error getting user'));
+app.post('/register', (req, res) => { 
+    register.handleRegister(req, res, db, bcrypt) 
 })
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
+app.get('/profile/:id', (req, res) => { 
+    profile.handleProfileGet(req, res, db)
 })
 
-//Password encryption using bcrypt node package
+app.put('/image', (req, res) => { 
+    image.handleImage(req, res, db) 
+})
+
+app.post('/imageurl', (req, res) => { 
+    image.handleApiCall(req, res) 
+})
+
+
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
